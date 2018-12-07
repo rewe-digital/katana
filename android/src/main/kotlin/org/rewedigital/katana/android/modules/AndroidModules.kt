64da515f66e8b7653dabb7242939e38342371d65
Sven Jacobs <sven.jacobs@rewe-digital.com>
2018-12-07 10:30:55 +0100

    Add Android module factory functions to Android artifact

---
+++ android/src/main/kotlin/org/rewedigital/katana/android/modules/AndroidModules.kt
@@ -0,0 +1,35 @@
+package org.rewedigital.katana.android.modules
+
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import org.rewedigital.katana.createModule
+
+const val APPLICATION = "APPLICATION"
+const val APPLICATION_CONTEXT = "APPLICATION_CONTEXT"
+
+/**
+ * Provides a named binding of `Application` as `APPLICATION` and of the application `Context` as `APPLICATION_CONTEXT`.
+ */
+fun createApplicationModule(app: Application) = createModule("applicationModule") {
+
+    bind<Application>(APPLICATION) { singleton { app } }
+
+    bind<Context>(APPLICATION_CONTEXT) { singleton { app } }
+}
+
+const val ACTIVITY = "ACTIVITY"
+const val ACTIVITY_CONTEXT = "ACTIVITY_CONTEXT"
+
+/**
+ * Provides a named binding of `Activity` as `ACTIVITY` and of the Activity's `Context` as `ACTIVITY_CONTEXT`.
+ *
+ * **Note:** This module should only be referenced from a [Component][org.rewedigital.katana.Component] located in an
+ * `Activity` or else Activity/memory leaks might be introduced!
+ */
+fun createActivityModule(activity: Activity) = createModule("activityModule") {
+
+    bind<Activity>(ACTIVITY) { singleton { activity } }
+
+    bind<Context>(ACTIVITY_CONTEXT) { singleton { activity } }
+}
