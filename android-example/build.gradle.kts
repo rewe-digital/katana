008e8924dccd8d9bf7a6293f73546ff0cdb8fb2a
Sven Jacobs <sven.jacobs@rewe-digital.com>
2018-12-14 14:42:51 +0100

    Update version references

--- android-example/build.gradle.kts
+++ android-example/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
         applicationId = "org.rewedigital.katana.android.example"
         versionCode = 1
-        versionName = "1.2.0"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
