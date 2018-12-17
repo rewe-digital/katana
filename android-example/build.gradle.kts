d4dcc436037b624f5e11cd11b93744c38eb231a6
Sven Jacobs <sven.jacobs@rewe-digital.com>
2018-12-17 09:22:22 +0100

    Update Android example app

--- android-example/build.gradle.kts
+++ android-example/build.gradle.kts
@@ -18,14 +18,14 @@ android {
 
         applicationId = "org.rewedigital.katana.android.example"
         versionCode = 1
-        versionName = "1.2.1"
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 }
 
 dependencies {
-    implementation("com.github.rewe-digital-incubator.katana:katana-android:1.2.1")
+    implementation("com.github.rewe-digital-incubator.katana:katana-android:1.2.2")
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.1")
@@ -37,10 +37,10 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.8.0")
     implementation("com.squareup.leakcanary:leakcanary-android:1.6.2")
 
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0")
-    androidTestImplementation("androidx.test:runner:1.1.0")
-    androidTestImplementation("androidx.test:rules:1.1.0")
-    androidTestImplementation("androidx.test.ext:junit:1.0.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
+    androidTestImplementation("androidx.test:runner:1.1.1")
+    androidTestImplementation("androidx.test:rules:1.1.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.0")
 }
 
 repositories {
