1e3ff87945691bac199a43c2afff4af3965615af
Sven Jacobs <sven.jacobs@rewe-digital.com>
2019-03-28 14:13:49 +0100

    Downgrade Spek to 2.0.0 because tests are not discovered
    https://github.com/spekframework/spek/issues/649

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -11,7 +11,7 @@ object Versions {
     const val gradleVersionsPlugin = "0.21.0"
     const val kluent = "1.49"
     const val kotlin = "1.3.21"
-    const val spek = "2.0.1"
+    const val spek = "2.0.0"
 }
 
 object Dependencies {
