c96d29f770d7ec5741e6d12ff93b434b0522e3a2
Sven Jacobs <sven.jacobs@rewe-digital.com>
2019-12-05 14:51:44 +0100

    Add optional injection via injectOrNull() / getOrNull()

--- core/src/main/kotlin/org/rewedigital/katana/dsl/ProviderDsl.kt
+++ core/src/main/kotlin/org/rewedigital/katana/dsl/ProviderDsl.kt
@@ -1,6 +1,7 @@
 package org.rewedigital.katana.dsl
 
 import org.rewedigital.katana.ComponentContext
+import org.rewedigital.katana.InjectionException
 
 /**
  * Provides syntax for injecting transitive dependencies via [get] within the provider body.
@@ -13,6 +14,20 @@ class ProviderDsl(val context: ComponentContext)
 /**
  * Provides a dependency which has already been declared in the current context (total set of modules of the
  * current component) to be able to inject transitive dependencies within a module.
+ *
+ * @return Requested dependency
+ * @throws InjectionException if no binding for dependency was found
+ * @see getOrNull
  */
 inline fun <reified T> ProviderDsl.get(name: Any? = null) =
     context.injectNow<T>(name = name, internal = true)
+
+/**
+ * Provides a dependency which has already been declared in the current context (total set of modules of the
+ * current component) to be able to inject transitive dependencies within a module.
+ *
+ * @return Requested dependency or `null` if no binding for dependency was found
+ * @see get
+ */
+inline fun <reified T> ProviderDsl.getOrNull(name: Any? = null) =
+    context.injectNowOrNull<T>(name = name, internal = true)
